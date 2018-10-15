058653822debd77317a4d967de878e366b0078e5
Amaobi Victor <aobikobe@gmail.com>
2018-10-16 00:53:11 +0100

    changed html page to see change

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = function (){
+
+	document.getElementById("attendant_log_in").onclick = function(){
+  		console.log("clicked 2");
+  	
+	
+	},
+	
+	document.getElementById("admin_log_in").onclick = function(){
+
+	//document.location.href = "attendants.html",true;
+	console.log("clicked 1");
+	}
+    
+  
+	
+	
+	
+ }
+
+
